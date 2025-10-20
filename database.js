466d472f6659b19c10843c6c913a38764ec08e97
nisaco <126338204+nisaco@users.noreply.github.com>
2025-10-20 23:52:04 +0000

    Update database.js

--- database.js
+++ database.js
@@ -37,5 +37,5 @@ const orderSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 const Order = mongoose.model('Order', orderSchema);
 
-// Export models AND the connection object
+// Export models and Mongoose instance
 module.exports = { User, Order, mongoose }; 
